97ac45efadb9ba749025f08f8e565de7722b8c14
Liu <1900018250@qq.com>
2018-07-28 16:58:18 +0800

    ll1

---
+++ op11/fgets2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define STLEN 10
+
+char * s_gets(char * st, int n);
+
+int main(int argc, char const *argv[])
+{
+    char words[STLEN];
+   // puts("enter empty line to quit");
+   // while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n')
+   //     fputs(words, stdout);
+   // puts("Done");
+    // s_gets(words, STLEN);
+    fgets(words, STLEN, stdin);
+
+    puts(words);
+    getchar();
+    return 0;
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    int i = 0;
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        while (st[i] != '\n' && st[i] != '\0')
+            i++;
+        printf("%d\n", i);
+        if (st[i] == '\n')
+            st[i] = '\0';
+        else
+        {
+            while (getchar() != '\n')
+            {
+                putchar('\n');
+                continue;
+            }
+        }
+    }
+}
--- op11/test.c
+++ op11/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define STLEN 14
 
 int main(int argc, char const *argv[])
 {
@@ -13,10 +14,16 @@ int main(int argc, char const *argv[])
         putchar(head[j]);
     putchar('\n');
     head = heart;
-    char * p1 = "Klingon";
-    p1[0] = 'F';
-    printf("Klingon");
-    printf("AA the %ss!\n", "Klingon");
+   // char * p1 = "Klingon";
+   // p1[0] = 'F';
+   // printf("Klingon");
+   // printf("AA the %ss!\n", "Klingon");
+    char words[STLEN];
+    puts("shu ru o ");
+    fgets(words, STLEN, stdin);
+    puts("aaa");
+    puts(words);
+    fputs(words, stdout);
     getchar();
     return 0;
 }
